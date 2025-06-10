1ef796eda3e4b4424171fe82e7153c01e11ee81a
Ritesh230087 <riteshdaas01@gmail.com>
2025-06-10 13:45:53 +0545

    crud completed

--- controllers/admin/usermanagement.js
+++ controllers/admin/usermanagement.js
@@ -50,3 +50,83 @@ exports.createUser = async (req, res) => {
     }
 }
 
+exports.getUsers = async (req, res ) => {
+    try{
+        const users = await User.find();
+        return res.status(200).json(
+            {
+                "success": true,
+                "message": "All users",
+                "data":users
+            }
+        )
+    }catch(err){
+        return res.status(500).json(
+            { "success": false, "message": "Server error" }
+        )
+    }
+}
+exports.getOneUser = async (req, res) => {
+    try{
+        const id = req.params.id 
+        const user = await User.findOne(
+            {
+                "_id": id
+            }
+        )
+        return res.status(200).json(
+            {
+                "succes": true,
+                "message": "One user fetched",
+                "data": user
+            }
+        )
+    }catch(err){
+        return res.status(500).json(
+            { "success": false, "message": "Server error" }
+        )
+    }
+}
+
+exports.updateOne = async (req, res) => {
+    const { firstName, lastName } = req.body
+    const _id = req.params.id // mongodb id
+    try{
+        const user = await User.updateOne(
+            {
+                "_id": _id
+            },
+            {
+                $set : {
+                    "firstName": firstName,
+                    "lastName": lastName
+                }
+            }
+        )
+        return res.status(200).json(
+            { "success": true, "message": "User updated"}
+        )
+    }catch(err){
+        return res.status(500).json(
+            { "success": false, "message": "Server error" }
+        )
+    }
+}
+
+exports.deleteOne = async (req, res) => {
+    const _id = req.params.id
+    try{
+        const user = await User.deleteOne(
+            {
+                "_id": _id
+            }
+        )
+        return res.status(200).json(
+            {"success" : true, "message": "User Deleted"}
+        )
+    }catch(err){
+        return res.status(500).json(
+            {"success": false, "message": "Server Error"}
+        )
+    }
+}
