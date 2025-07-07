d535b298c5b5849c4edaf073cebfb87f9d7a5272
Ritesh230087 <riteshdaas01@gmail.com>
2025-07-07 15:54:00 +0545

    Forget Passowrd Route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,9 +7,16 @@ router.post(
     "/register",
     registerUser
 )
-
 router.post(
     "/login",
     loginUser
+)
+router.post(
+    "/request-reset",
+     sendResetLink
+)
+router.post(
+    "/reset-password/:token", 
+    resetPassword
 )
 module.exports = router
