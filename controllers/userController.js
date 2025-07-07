24f4a42fcda3bdef40b01795f5832e5b0297bef0
Ritesh230087 <riteshdaas01@gmail.com>
2025-07-07 15:51:34 +0545

    Forget Passowrd controller done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require("../models/UserModels");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { v4: uuidv4 } = require("uuid"); 
+const nodemailer = require("nodemailer")
 
 // Register user
 exports.registerUser = async (req, res) => {
@@ -103,3 +103,48 @@ exports.loginUser = async (req, res) => {
         });
     }
 };
+const transpoter = nodemailer.createTransport(
+    {
+        service: "gmail",
+        auth:{
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    }
+)
+
+exports.sendResetLink = async (req, res) => {
+    const { email } = req.body
+    try{
+        const user = await User.findOne({ email })
+        if(!user) return res.status(404).json({success: false, message: "User not found"})
+        const token = jwt.sign({id: user._id}, process.env.SECRET, {expiresIn: "20m"})
+        const resetUrl = process.env.CLIENT_URL + "/reset-password/" + token
+        const mailOptions = {
+            from: `"Your app" <${process.env.EMAIL_USER}>`, // backtick
+            to: email,
+            subject: "Reset your password",
+            html: `<p>Click on the link to reset.. ${resetUrl}</p>`
+        }
+        transpoter.sendMail( mailOptions, (err, info) => {
+            if(err) return res.status(403).json({success: false, message: "Email failed"})
+            console.log(info)
+            return res.status(200).json({success: true, message: "Email sent"})
+        })
+    }catch(err){
+        return res.status(500).json({success: false, message: "Server err"})
+    }
+}
+
+exports.resetPassword = async  (req, res) => {
+    const { token } = req.params;
+    const { password } = req.body
+    try{
+        const decoded = jwt.verify(token, process.env.SECRET)
+        const hashed = await bcrypt.hash(password, 10)
+        await User.findByIdAndUpdate(decoded.id, { password: hashed })
+        return res.status(200).json({ success: true, message: "Password updated" })
+    }catch(err){
+        return res.status(500).json({success: false, message: "Server err/Invalid token"})
+    }
+}
