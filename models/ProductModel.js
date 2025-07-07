1ad6d193d34cbfba2571b2ec56a84f6396c5ca3c
Ritesh230087 <riteshdaas01@gmail.com>
2025-07-07 14:35:17 +0545

    ProductModel

---
+++ models/ProductModel.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String, 
+            required: true
+        },
+        description: {
+            type: String, 
+            required: true 
+        },
+        price: { 
+            type: Number,
+            required: true 
+        }, 
+        originalPrice: { 
+            type: Number,
+             required: true
+        }, 
+        discountPercent: { 
+            type: Number
+        },
+        youSave: { 
+            type: Number
+        },
+        quantity: { 
+            type: Number, 
+            required: true 
+        },
+        filepath: { 
+            type: String, 
+            required: true 
+        }, 
+        categoryId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Category",
+            required: true
+        },
+        ribbonId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Ribbon",
+            default: null
+        }
+    }, 
+    { 
+        timestamps: true 
+    }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
