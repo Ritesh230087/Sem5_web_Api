691e0e87e4dcccf0180c17d731e443d6acbff8a2
Ritesh230087 <riteshdaas01@gmail.com>
2025-07-17 02:49:05 +0545

    order route

---
+++ routes/orderRoute.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  createOrder,
+  createOrderWithSlip,
+  getMyOrderById,
+  getLastShippingAddress
+} = require('../controllers/orderController');
+
+const uploader = require('../middlewares/fileupload');
+const { authenticateUser } = require('../middlewares/authorizedUsers');
+router.post('/create', authenticateUser, createOrder);
+router.post('/create-with-slip', authenticateUser, uploader.single('slipImage'), createOrderWithSlip);
+router.get('/myorders/:id', authenticateUser, getMyOrderById);
+router.get('/last-shipping/:userId', authenticateUser, getLastShippingAddress);
+
+module.exports = router;
