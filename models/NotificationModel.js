056fe03836fc617a93a41fd1b22c6017836eb57e
Ritesh230087 <riteshdaas01@gmail.com>
2025-07-17 02:45:45 +0545

    notification model

--- models/NotificationModel.js
+++ models/NotificationModel.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const notificationSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   message: { type: String, required: true },
-  link: { type: String }, // e.g., '/orders/your_order_id'
+  link: { type: String }, 
   isRead: { type: Boolean, default: false },
+  category: { type: String, enum: ['order', 'promotion', 'account', 'general'], default: 'general'}
 }, { timestamps: true });
 
 module.exports = mongoose.model('Notification', notificationSchema);
